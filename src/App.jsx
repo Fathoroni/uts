a3587f3eede0cd9847447edbd980960948d775ff
Fathoroni <fathoroni42@gmail.com>
2024-05-04 16:37:53 +0700

    Pertama commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import {StyleSheet, Text, View, FlatList, Settings} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Home from './screens/Home';
+import About from './screens/About';
+import Account from './screens/Account';
+
+const Stack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+const MenuTab = () => {
+  return (
+    <Tabs.Navigator screenOptions={{tabBarActiveTintColor: 'green'}}>
+      <Tabs.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="About"
+        component={About}
+        options={{
+          tabBarLabel: 'About',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="information-variant" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Account"
+        component={Account}
+        options={{
+          tabBarLabel: 'Account',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs.Navigator>
+  );
+};
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Tab"
+          component={MenuTab}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+const style = StyleSheet.create({});
