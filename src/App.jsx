c8e51993cb97bec8d9d3627e44d188f2c5f9ea6d
Fathoroni <fathoroni42@gmail.com>
2024-05-04 19:57:47 +0700

    commit kedua

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,74 @@
-import {StyleSheet, Text, View, FlatList, Settings} from 'react-native';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { StyleSheet, NavigationContainer } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from './screens/Home';
 import About from './screens/About';
 import Account from './screens/Account';
+import { theme } from './theme';
 
 const Stack = createNativeStackNavigator();
 const Tabs = createBottomTabNavigator();
 
+const TabScreen = ({ name, component, icon }) => {
+  return (
+    <Tabs.Screen
+      name={name}
+      component={component}
+      options={{
+        tabBarLabel: name,
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name={icon} color={color} size={size} />
+        ),
+      }}
+    />
+  );
+};
+
 const MenuTab = () => {
   return (
-    <Tabs.Navigator screenOptions={{tabBarActiveTintColor: 'green'}}>
-      <Tabs.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="About"
-        component={About}
-        options={{
-          tabBarLabel: 'About',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="information-variant" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="Account"
-        component={Account}
-        options={{
-          tabBarLabel: 'Account',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }}
-      />
+    <Tabs.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: theme.colors.active,
+        tabBarStyle: styles.tabBar,
+      }}
+    >
+      <TabScreen name="Home" component={Home} icon="home" />
+      <TabScreen name="About" component={About} icon="information-variant" />
+      <TabScreen name="Account" component={Account} icon="account" />
     </Tabs.Navigator>
   );
 };
 
 function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer style={styles.container}>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Tab"
-          component={MenuTab}
-          options={{headerShown: false}}
-        />
+        <Stack.Screen name="Tab" component={MenuTab} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+  },
+  tabBar: {
+    backgroundColor: '#fff',
+    borderTopWidth: 1,
+    borderTopColor: '#ddd',
+    height: 60,
+  },
+});
+
 export default App;
 
-const style = StyleSheet.create({});
+export const theme = {
+  colors: {
+    active: 'green',
+  },
+};
+
